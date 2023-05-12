95c491b38e6f5fe3d0c31d8b2001581758ff31f9
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-05-12 18:28:57 +0500

    render two pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Categories from './pages/Categories';
+import Home from './pages/home';
+import Navbar from './components/navbar';
+
 function App() {
-  return <h1>hy</h1>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
