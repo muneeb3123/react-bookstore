fb786c464c2df731bb396957787087c0ed9ff925
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-05-18 21:35:54 +0500

    add loading state

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,13 @@
+import { useSelector } from 'react-redux';
 import Booklist from '../components/booklist';
 import BookForm from '../components/bookform';
 
 function Home() {
+  const { isLoading } = useSelector((state) => state.book);
+
+  if (isLoading) {
+    return <h1>loading....</h1>;
+  }
   return (
     <div>
       <Booklist />
