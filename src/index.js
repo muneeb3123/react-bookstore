61b5bd1a32b3866d2b8070c8cbce21f231d42d31
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-05-18 21:34:39 +0500

    dispatch getBook

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import './index.css';
 import App from './App';
+import { getBook } from './redux/books/booksSlice';
+
+store.dispatch(getBook());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
